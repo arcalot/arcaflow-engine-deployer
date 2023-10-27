f70541a291865cfcdc3d128092f63a632995afb3
Jared O'Connell <joconnel@redhat.com>
2023-10-27 17:48:01 -0400

    Progress towards multiple deployment types

--- registry/new.go
+++ registry/new.go
@@ -8,13 +8,21 @@ import (
 
 // New creates a new registry with the given factories.
 func New(factory ...deployer.AnyConnectorFactory) Registry {
-	factories := make(map[string]deployer.AnyConnectorFactory, len(factory))
+	factories := make(map[deployer.DeploymentType]map[string]deployer.AnyConnectorFactory, len(factory))
 
 	for _, f := range factory {
-		if v, ok := factories[f.ID()]; ok {
-			panic(fmt.Errorf("duplicate deployer factory ID: %s (first: %T, second: %T)", f.ID(), v, f))
+		deploymentType := f.DeploymentType()
+		category, categoryCreated := factories[deploymentType]
+		if !categoryCreated {
+			category = make(map[string]deployer.AnyConnectorFactory)
+			factories[deploymentType] = category
 		}
-		factories[f.ID()] = f
+
+		if v, ok := category[f.ID()]; ok {
+			panic(fmt.Errorf("duplicate deployer factory ID for deployment type %s: %s (first: %T, second: %T)",
+				deploymentType, f.ID(), v, f))
+		}
+		category[f.ID()] = f
 	}
 
 	return &registry{
